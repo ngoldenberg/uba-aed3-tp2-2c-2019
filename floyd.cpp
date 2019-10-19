909c335a4d5b687f33bd7978b6efbccacafab84c
Franco Armando Martinez Quispe <franco.martinez93@hotmail.com>
2019-10-18 21:56:57 -0300

    un cambio

--- floyd.cpp
+++ floyd.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <iostream>
 #include <vector>
+#include <math.h>       
 
 using namespace std;
 
@@ -30,6 +31,7 @@ int main()
 		}
 	}
 
+	vector<double> div;
 	cout << floydWarshall(G,n) << endl;
 	return 0;
 }
@@ -44,12 +46,12 @@ bool floydWarshall (Grafo G, uint n)
     {
         for (uint j = 0; j < n; j++)  
         {   
-        	dist[i][j] = G[i][j]; 
+        	dist[i][j] = log(G[i][j]); 
+        	cout<<dist[i][j]<<"     "; 
         }
+        cout<<endl;
     }
-
-
-    
+    cout << endl; 
 
     for (uint k = 0; k < n; k++)  
     {  
@@ -57,15 +59,21 @@ bool floydWarshall (Grafo G, uint n)
         for (uint i = 0; i < n; i++)  
         {  
             // Pick all vertices as destination for the  
-            // above picked source  
+            // above picked source 
+           /*if(dist[i][k] + dist[k][i] < 0)
+            {	cout << dist[i][k] + dist[k][i] <<endl; 
+            	return false;
+            }*/
             for (uint j = 0; j < n; j++)  
             {  
                 // If vertex k is on the shortest path from  
                 // i to j, then update the value of dist[i][j]  
-                double d = dist[i][k]*dist[k][j];
-                if (dist[i][j] < d){ 
-                    dist[i][j] = d; 
-                    } 
+               /* double d = dist[i][k] + dist[k][j];
+                if (dist[i][j] > d){ 
+                    dist[i][j] = d;
+
+                    } */
+            	dist[i][j] = max(dist[i][j], dist[i][k] + dist[k][j]);
             }  
         }
         cout << "k: " << k <<endl;
@@ -82,5 +90,5 @@ bool floydWarshall (Grafo G, uint n)
 
     
 
-    return dist[0][0] > 1;    
+    return dist[0][0] > 0;    
 }
