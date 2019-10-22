2ec67fef691b8a32c20dffb0f6d0552504618bf6
Nicolas Goldenberg <nicogol0@gmail.com>
2019-10-21 22:27:13 -0300

    Add new constants

--- src/ejercicio_2/config/constants.h
+++ src/ejercicio_2/config/constants.h
@@ -4,7 +4,11 @@
 #include <string>
 #include <memory>
 #include <vector>
+#include <list>
 
 using namespace std;
 
 typedef vector<vector<float>> FloatMatrix;
+
+typedef vector<list<pair < int, float>>>
+AdjList;
