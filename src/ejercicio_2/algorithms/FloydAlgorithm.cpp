12ad84a044625ceac0843ca44616c3d9216baada
Nicolas Goldenberg <nicolas.p.goldenberg@gmail.com>
2019-10-23 18:52:38 -0300

    Merge Floyd with Algorithm interface

---
+++ src/ejercicio_2/algorithms/FloydAlgorithm.cpp
@@ -0,0 +1,41 @@
+#include "FloydAlgorithm.h"
+
+
+vector<int> FloydAlgorithm::Solve(int const n, FloatMatrix const &matrix) {
+  double dist[n][n] = {0};
+  int pred[n][n];
+  memset(pred, 0, sizeof(pred));
+
+  for (uint i = 0; i < n; i++) {
+    for (uint j = 0; j < n; j++) {
+      dist[i][j] = log(matrix[i][j]);
+      pred[i][j] = i;
+    }
+  }
+
+  for (uint k = 0; k < n; k++) {
+    for (uint i = 0; i < n; i++) {
+      for (uint j = 0; j < n; j++) {
+        double d = dist[i][k] + dist[k][j];
+        if (d > dist[i][j]) {
+          dist[i][j] = d;
+          pred[i][j] = pred[k][j];
+        }
+      }
+    }
+  }
+
+  if (dist[0][0] <= 0) {
+    throw logic_error("No negative cycle detected");
+  }
+
+  vector<int> cycle;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (i == pred[0][j])
+        cycle.push_back(matrix[i][j]);
+    }
+  }
+
+  return cycle;
+}
---
+++ src/ejercicio_2/algorithms/FloydAlgorithm.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <vector>
+
+#include "Algorithm.h"
+#include "config/constants.h"
+
+class FloydAlgorithm : public Algorithm {
+ public:
+  FloydAlgorithm() {};
+  ~FloydAlgorithm() {};
+  vector<int> Solve(int const currencies_quantity, FloatMatrix const &matrix) override;
+};
+
