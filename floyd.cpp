354550d835ed577a593131e4e3af661f7ac58c68
fquispe <franco.martinez93@hotmail.com>
2019-10-23 01:13:30 -0300

    floyd listo

--- floyd.cpp
+++ floyd.cpp
@@ -1,127 +1,95 @@
-#include <algorithm>
 #include <cmath>
 #include <iostream>
 #include <vector>
 #include <math.h> 
 #include <string>      
+#include <cstring>    
 
 using namespace std;
 
 typedef vector<vector<double>> Grafo;
 
 
-bool floydWarshall (Grafo G, uint n);
+void floydWarshall (Grafo G, uint n);
 
 int main()
 {
 	uint n;
 
-	cin >> n;
-	Grafo G(n, vector<double>(n, 1));
+    cin >> n;
+    Grafo G(n, vector<double>(n, 1));
 
-	for(uint i = 0; i < n; i++)
-	{
-		for (uint j = 0; j < n; ++j)
-		{
-			double c;
-			cin >> c;
-			if(i != j)
-			{
-				G[i][j] = c;
-			}
-		}
-	}
+    for(uint i = 0; i < n; i++)
+    {
+        for (uint j = 0; j < n; ++j)
+        {
+            double c;
+            cin >> c;
+            if(i != j)
+            {
+                G[i][j] = c;
+            }
+        }
+    }
 
-	vector<double> div;
-	cout << floydWarshall(G,n) << endl;
-	return 0;
+    floydWarshall(G,n);
+    return 0;
 }
 
 
-bool floydWarshall (Grafo G, uint n)
+void floydWarshall (Grafo G, uint n)
 {
-	double dist[n][n][n];
-	double path[n][n][n] = {0};
+  
+    double dist[n][n] = {0};
+    int pred[n][n];
+    memset(pred,0,sizeof(pred));
   
 
     for (uint i = 0; i < n; i++)  
     {
         for (uint j = 0; j < n; j++)  
         {   
-            dist[i][j][0] = log(G[i][j]); 
-        	cout<<dist[i][j][0]<<"     "; 
-        	path[i][j][0] = i;
+            dist[i][j] = log(G[i][j]); 
+         //   cout<< dist[i][j] <<"     "; 
+            pred[i][j] = i;
         }
-        cout<<endl;
+       // cout<<endl;
     }
-    cout << endl; 
+//    cout << endl; 
+    
 
-    for (int s = 1; s < n; ++s)
-    {
-     
-        for (uint k = 0; k < n; k++)  
+    for (uint k = 0; k < n; k++)  
+    {  
+        for (uint i = 0; i < n; i++)  
         {  
-            for (uint i = 0; i < n; i++)  
-            {  
-            
-                for (uint j = 0; j < n; j++)  
-                {  
-            
-                    double d = dist[i][k][s-1] + dist[k][j][0];
-                    if (dist[i][j][s] < d){ 
-               		   dist[i][j][s] = d;
-               		   path[i][j][s] = k; 
-                    } 
+            for (uint j = 0; j < n; j++)  
+            {        
+                double d = dist[i][k] + dist[k][j];
+                if (d > dist[i][j]){ 
+                    dist[i][j] = d;
+                    pred[i][j] = pred[k][j]; 
+                } 
            
-                }  
-            }
-           /* cout << "k: " << k <<endl;
-            for (uint i = 0; i < n; i++)  
-   		   { 
-        	   for (uint j = 0; j < n; j++)  
-        	   {  
-        		  cout<<path[i][j][s]<<"     ";  
-        	   }  
-        	   cout<<endl;  
-    	   }
-    	   cout << endl;*/    
-        }
+            }  
+        }    
+    }
+
+    if(dist[0][0] <= 0)
+    {
+        cout << "NO" << endl;
+        return;
     }
     
-    int s, arb = -1;
-    for (s; s < n; ++s)
+    cout << "SI";    
+    for (int i = 0; i < n; i++)
     {
-        for (int i = 0; i < n; ++i)
+      for (int j = 0; j < n; j++)
         {
-            if(dist[i][i][s] > 0)
-            {
-                arb = i;
-            }
-            break;
+          if (i == pred[0][j])
+            cout << " " << G[i][j];
         }
-        if (arb != -1)
-        {
-            break;
-        }
-    }
-
-    for (int s = 0; s < n; ++s)
-    {
-        cout << "s: " << s << endl; 
-        for (int i = 0; i < n; ++i)
-        {
-            for (int j = 0; j < n; ++j)
-            {
-                cout << "("<< dist[i][j][s] << ", "<< path[i][j][s] << ") ";
-            }
-            cout << endl;
-        }
-    }
-
-    if(arb != -1)
-    {
-        vector<double> p;
-    }
-
-    return "NO";    
+    } 
+        
+    cout  << endl;
+  
 }
