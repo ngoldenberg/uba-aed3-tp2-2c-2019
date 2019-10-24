00c835c67cc84d912941a8cd164c6b81a48823f2
Nicolas Goldenberg <nicogol0@gmail.com>
2019-10-24 17:28:01 -0300

    Add random cases for arbitraje

--- profiling/arbitraje_random_cases.py
+++ profiling/arbitraje_random_cases.py
@@ -3,33 +3,35 @@ from tools.profiler import profile_instance
 from tools.record_tools import generate_records
 from numpy.random import RandomState
 
-from instancegenerators.uniform_elements import generate_unifrom_elements
+from instancegenerators.uniform_elements import generate_uniform_elements
 
-SEED_WEIGHTS = 97884319
-SEED_PROFITS = 91558205
-
-SEED_W = 58174575
-
+SEED_MULTIPLIER = 97884319
 SEED_DEVIATION = 17051314
 SEED_SPREAD = 92927118
 
-W_random = RandomState(SEED_W)
 deviation_random = RandomState(SEED_DEVIATION)
 spread_random = RandomState(SEED_SPREAD)
-weights_random = RandomState(SEED_WEIGHTS)
-profits_random = RandomState(SEED_PROFITS)
+multiplier_random = RandomState(SEED_MULTIPLIER)
 
-def gen_instance(index):
-    return (generate_unifrom_elements(n=1000, W=1000, weight_deviation=deviation_random.uniform(0.0,0.1), weight_spread=spread_random.uniform(0.0,1.0), next_weight=weights_random.randint, next_profit=profits_random.randint), "bf", 30)
+
+def gen_instance(index, algorithm):
+    return (generate_uniform_elements(n=3, multiplier_deviation=deviation_random.uniform(1.0, 50),
+                                      gain_spread_percentage=spread_random.uniform(1, 100),
+                                      next_multiplier=multiplier_random.uniform), algorithm is "Floyd", 30)
+
+
+def gen_algorithm(i):
+    return ['Floyd', 'BellmanFord']
+
 
 write_csv('results/arbitraje_random_cases.csv',
-    generate_records(
-        dimensions=[
-            ('n', lambda: range(0, 100))
-        ],
-        generators=[
-            ('algorithm_time', gen_instance)
-        ]
-    )
-)
-
+          generate_records(
+              dimensions=[
+                  ('instance_index', lambda: range(0, 100)),
+                  ('algorithm', gen_algorithm)
+              ],
+              generators=[
+                  ('algorithm_time', gen_instance)
+              ]
+          )
+          )
