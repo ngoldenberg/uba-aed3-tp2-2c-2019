2d630668f0ed7099c74608b5a1f45f2c38d55d77
Nicolas Goldenberg <nicogol0@gmail.com>
2019-10-24 14:50:14 -0300

    Add Algorithm Input

--- profiling/instancegenerators/program_input.py
+++ profiling/instancegenerators/program_input.py
@@ -1,20 +1,21 @@
 from itertools import chain
 
-class Order:
-    def __init__(self, weight, profit):
-        self.weight = weight
-        self.profit = profit
-
-class ProgramInput:
-    def __init__(self, order_set, w):
-        self.order_set = order_set
-        self.w = w
+class AlgorithmInput:
+    def __init__(self, n, matrix):
+        self.n = n
+        self.matrix = matrix
 
     def get_header_lines(self):
-        return [str(len(self.order_set)) + " " + str(self.w)]
+        return [str(self.n)]
 
     def get_element_lines(self):
-        return (str(elem.weight) + " " + str(elem.profit) for elem in self.order_set)
+        lines = []
+        for row in self.matrix:
+            row_values = []
+            for column in row:
+                row_values.append(column)
+            lines.append(" ".join(row_values) + "\n")
+        return lines
 
     def __str__(self):
         return "\n".join(chain(self.get_header_lines(), self.get_element_lines()))
