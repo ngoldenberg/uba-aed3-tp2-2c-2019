878fa12ca1c5fc998b02d9fff7c1ee0def0f8f74
franketi01 <francogm01@gmail.com>
2019-12-01 19:08:21 -0300

    floyd fix

--- src/ejercicio_2/algorithms/FloydAlgorithm.cpp
+++ src/ejercicio_2/algorithms/FloydAlgorithm.cpp
@@ -2,44 +2,41 @@
 
 vector<int> FloydAlgorithm::Solve(int const n, FloatMatrix const &matrix) {
   double dist[n][n] = {0};
-  int pred[n][n];
-  memset(pred, 0, sizeof(pred));
+  int next[n][n];
+  memset(next, 0, sizeof(next));
 
   for (uint i = 0; i < n; i++) {
     for (uint j = 0; j < n; j++) {
-      if (i == j) {
-        continue;
-      }
-      dist[i][j] = log(matrix[i][j]);
-      pred[i][j] = i;
+ 
+      dist[i][j] = log(1/matrix[i][j]);
+      next[i][j] = j;
     }
   }
 
   for (uint k = 0; k < n; k++) {
     for (uint i = 0; i < n; i++) {
       for (uint j = 0; j < n; j++) {
-        if (k == j || k == i) {
-          continue;
-        }
         double d = dist[i][k] + dist[k][j];
-        if (d > dist[i][j]) {
+        if (dist[i][j] > d) {
           dist[i][j] = d;
-          pred[i][j] = pred[k][j];
+          next[i][j] = next[i][k];
         }
       }
     }
   }
 
-  if (dist[0][0] <= 0) {
+  if (dist[0][0] >= 0) {
     throw logic_error("No negative cycle detected");
   }
 
   vector<int> cycle;
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      if (i == pred[0][j])
+  cycle.push_back(0);
+  uint i = 0;
+  do{
+      uint j = next[next_node][0];
         cycle.push_back(GetEdgeNumberByAxis(matrix, i, j));
-    }
+      i = j;
+    }while(i != 0);
   }
 
   return cycle;
